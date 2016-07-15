9a97f111b5f48235e97fbb728a0b83580df45584
Clay Stewart <claytonmstewart@gmail.com>
2016-07-15 01:37:18 -0500

    add uglified files for 1.0.9

--- dist/ng-dynamic-title.js
+++ dist/ng-dynamic-title.js
@@ -10,16 +10,18 @@ angular.module('ng.dynamic-title').directive('ngDynamicTitle',[
     function link($scope, $element) {
       function stateChangeCallback(event, toState) {
         var title;
+        $scope.separator = $scope.separator ? $scope.separator : '-';
+        $scope.separatorTagline = $scope.separatorTagline ? $scope.separatorTagline : '-';
 
         // build default title suffix
         if($scope.rootTitle && $scope.subTitle){
-          title = $scope.rootTitle+' | '+$scope.subTitle;
+          title = $scope.rootTitle+' '+$scope.separator+' '+$scope.subTitle;
         }
 
         // either prepend toState.data.ngDynamicTitle default scope titles
         // or display the title on toState.data.ngDynamicTitle
         if (toState.data && toState.data.ngDynamicTitle && title) {
-          title = toState.data.ngDynamicTitle + ' | ' + title;
+          title = toState.data.ngDynamicTitle + ' '+$scope.separator+' ' + title;
         } else {
           title = toState.data.ngDynamicTitle;
         }
@@ -38,7 +40,9 @@ angular.module('ng.dynamic-title').directive('ngDynamicTitle',[
       link: link,
       scope: {
         'rootTitle': '@',
-        'subTitle': '@'
+        'subTitle': '@',
+        'separator': '@?',
+        'separatorTagline': '@?'
       }
     };
 
